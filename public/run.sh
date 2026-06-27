3116e86c7a9a486db55c740d8ba095fecc8f6b1e
josh <96921768+joshwakelin@users.noreply.github.com>
2026-06-27 18:48:10 -0400

    Decrease sleep time from 0.3s to 0.05s
    Reduce sleep duration between opening GIFs.

--- public/run.sh
+++ public/run.sh
@@ -6,7 +6,7 @@ done
 
 for i in {1..20}; do
   open ~/Downloads/hamster-$i.gif
-  sleep 0.3
+  sleep 0.05
   x=$((RANDOM % 1400))
   y=$((RANDOM % 700))
   osascript -e "tell app \"Preview\" to set bounds of front window to {$x, $y, $((x+465)), $((y+498))}"
