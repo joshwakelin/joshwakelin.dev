15671df24b458b4d34bada59bba790ca29ff9133
josh <96921768+joshwakelin@users.noreply.github.com>
2026-06-27 18:44:05 -0400

    Add script to download and display hamster GIFs
    The script downloads 20 hamster GIFs and opens them in Preview with random positioning.

---
+++ public/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for i in {1..20}; do
+  curl -L -o ~/Downloads/hamster-$i.gif 'https://media1.tenor.com/m/yDZ_I9QUMOgAAAAC/happy-hamster.gif'
+done
+
+for i in {1..20}; do
+  open ~/Downloads/hamster-$i.gif
+  sleep 0.3
+  x=$((RANDOM % 1400))
+  y=$((RANDOM % 700))
+  osascript -e "tell app \"Preview\" to set bounds of front window to {$x, $y, $((x+465)), $((y+498))}"
+done
